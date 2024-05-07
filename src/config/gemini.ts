e16d8ac13bb0c9d3833a6a22707b4677f8612f65
n4ze3m <n4ze3m@gmail.com>
2024-05-07 16:51:34 +0530

    chore: Update gitignore and package.json for documentation files

--- src/config/gemini.ts
+++ src/config/gemini.ts
@@ -18,13 +18,4 @@ export const DEFAULT_GOOGLE_GEMINI_MODELS: ModelConfig[] = [
     type: "chat",
     context_size: 32_000
   },
-  {
-    model_id: "gemini-1.5-pro-latest-dialoq",
-    name: "Gemini 1.5 Pro",
-    fucntion_call: false,
-    provider: "google",
-    vision: true,
-    type: "chat",
-    context_size: 1_000_000
-  },
 ]
