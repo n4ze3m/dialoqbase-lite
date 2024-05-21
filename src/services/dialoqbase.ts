7123a14c1a3e6c00b6f0b0a624fe00ca223e18f0
Ginto <ginto.sakata@gmail.com>
2024-05-21 17:51:00 +0300

    fixed a typo in constant name

--- src/services/dialoqbase.ts
+++ src/services/dialoqbase.ts
@@ -8,7 +8,7 @@ const DEFAULT_PAGE_SHARE_URL = "https://pageassist.xyz"
 const DEFAULT_RAG_QUESTION_PROMPT =
   "Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.   Chat History: {chat_history} Follow Up Input: {question} Standalone question:"
 
-const DEFAUTL_RAG_SYSTEM_PROMPT = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end. If you don't know the answer, just say you don't know. DO NOT try to make up an answer. If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.  
+const DEFAULT_RAG_SYSTEM_PROMPT = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end. If you don't know the answer, just say you don't know. DO NOT try to make up an answer. If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.  
 
 Context:
 
@@ -56,7 +56,7 @@ export const promptForRag = async () => {
   let ragQuestionPrompt = questionPrompt
 
   if (!ragPrompt || ragPrompt.length === 0) {
-    ragPrompt = DEFAUTL_RAG_SYSTEM_PROMPT
+    ragPrompt = DEFAULT_RAG_SYSTEM_PROMPT
   }
 
   if (!ragQuestionPrompt || ragQuestionPrompt.length === 0) {
